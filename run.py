bc853e17de1740819b850280ebe34fe569d6a769
Fotis Kapotos <fotiskapotos@gmail.com>
2018-08-26 01:27:11 +0200

    Add an MIT License

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import argparse
 import utils
 
+# Initialize the Parser and add the argument
 ap=argparse.ArgumentParser(description="Arguments for running the visualisation script")
-
 ap.add_argument('-s','--list',type=float,dest='s', nargs='+',help="The different sinuses to construct the signal", required=True)
 
+# Get the different sinuses
 pa = ap.parse_args()
 sinuses = pa.s
-print(sinuses)
-s , t = utils.signal(sinuses)
-utils.Fourier(s,t)
+
+
+s , t = utils.signal(sinuses) # Create, plot and save the signal
+utils.Fourier(s,t) # Decompose it, plot it and save it
