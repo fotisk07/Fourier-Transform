3b14b7b8a16675ef7581441fdd2bce7cd4be2e78
Fotis Kapotos <fotiskapotos@gmail.com>
2018-08-26 14:45:40 +0200

    Added CMD functionality for using my own FFT

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ import utils
 # Initialize the Parser and add the argument
 ap=argparse.ArgumentParser(description="Arguments for running the visualisation script")
 ap.add_argument('-s','--list',type=float,dest='s', nargs='+',help="The different sinuses to construct the signal", required=True)
+ap.add_argument("--n", type=str, dest="n",help="A boolean value to sepcify whether to use my own implementation of the FFT(True) or the numpy command(False)", default = "False")
 
 # Get the different sinuses
 pa = ap.parse_args()
 sinuses = pa.s
+n = pa.n
 
 
 s , t = utils.signal(sinuses) # Create, plot and save the signal
-utils.Fourier(s,t) # Decompose it, plot it and save it
+utils.Fourier(s, t, n) # Decompose it, plot it and save it
